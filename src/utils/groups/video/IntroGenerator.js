62bc14d2bcaba165cec3ca2e5cfa15806f4a3013
Just-Prog <JustProg5835@outlook.com>
2024-06-21 07:50:52 +0800

    基于现行的后端规格添加了团播&双播的判断 后期可能会改为飞书组织日历API抓取

--- src/utils/groups/video/IntroGenerator.js
+++ src/utils/groups/video/IntroGenerator.js
@@ -3,18 +3,39 @@ function IntroGen(info){
     let e = info.extendedProps;
     let r = "";
     if(e.live_type==0){
-        r = e.live_room
+        r = e.live_room;
+    }else if(e.live_type==3){
+        r = e.live_room + '&' + e.partner.shortName;
     }else{
-        r = 'A-SOUL'
+        r = 'A-SOUL';
     }
     let t = e.pure_title;
     let t_ = new Date(info.start.getTime() - 8 * 60000 * 60);
     let d = t_.getFullYear()+"年"+(t_.getMonth()+1)+"月"+t_.getDate()+"日";
-    let f = e.fullname;
-    let s = e.space;
-    let a = `直播日期：${d}\n${r}【${t}】\n--------------------------------------------------------\n${f}\n直播间：${info.url}/\n个人主页：https://space.bilibili.com/${s}/`;
-    // todo
-    // 团播双播简介模板
+    let a = `直播日期：${d}\n${r}【${t}】\n--------------------------------------------------------\n`;
+    if(e.live_type==1){
+        a += `贝拉kira
+直播间：https://live.bilibili.com/22632424/
+个人主页：https://space.bilibili.com/672353429/
+
+嘉然今天吃什么
+直播间：https://live.bilibili.com/22637261/
+个人主页：https://space.bilibili.com/672328094/
+
+乃琳Queen
+直播间：https://live.bilibili.com/22625027/
+个人主页：https://space.bilibili.com/672342685/`;
+    }else{
+        let f = e.fullname;
+        let s = e.space;
+        a += `${f}\n直播间：${info.url}/\n个人主页：https://space.bilibili.com/${s}/`;
+        if(e.live_type==3){
+            let q = e.partner.fullname;
+            let x = e.partner.space;
+            let r = e.partner.liveRoom;
+            a += `\n\n${q}\n直播间：https://live.bilibili.com/${r}/\n个人主页：https://space.bilibili.com/${x}/`;
+        }
+    }
     return a;
 }
 export default IntroGen
