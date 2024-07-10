5a75b8a7389fa87921fd9d8c0bf3615fc4e72956
Just-Prog <JustProg5835@outlook.com>
2024-07-10 22:30:59 +0800

    飞书日程: 重写modal提醒，附加当日直播间等信息

--- src/components/Lark/LarkCalendar.vue
+++ src/components/Lark/LarkCalendar.vue
@@ -3,21 +3,23 @@ import { get_uat as get } from '@/utils/requests/uat_proxy'
 import FullCalendar from '@fullcalendar/vue3'
 import listPlugin from '@fullcalendar/list'
 import interactionPlugin from '@fullcalendar/interaction'
-import { Modal } from '@arco-design/web-vue'
 import { onMounted, ref } from 'vue';
 import IntroGen from '@/utils/groups/video/IntroGeneratorLark';
 const lark_cal = ref([])
 const calendarLoading = ref(true)
-// const getLarkCalendar = ()=>{
-//     // requests.get('/api/lark_calendar_list').then(resp=>{
-//     //     // lark_cal.value = resp.data
-//     //     for(let i=0;i<resp.data.length;i++){
-//     //         lark_cal.value.push(resp.data[i])
-//     //     }
-//     // }).catch(e=>{
-//     //     console.log(e)
-//     // })
-// }
+const IntroModalVisible = ref(false)
+const IntroModalLR = ref("")
+const IntroModalContent = ref("")
+const IntroModalHide = ()=>{
+    IntroModalVisible.value = false;
+}
+const IntroModalLRCopy = () => {
+    navigator.clipboard.writeText(IntroModalLR.value)
+}
+const IntroModalContentCopy = () => {
+    navigator.clipboard.writeText(IntroModalContent.value)
+    IntroModalHide()
+}
 async function getLarkCalendar (){
     await get('/api/lark_calendar_list').then(resp=>{
         for(let i=0;i<resp.length;i++){
@@ -41,18 +43,10 @@ const calendarOptions = {
     firstDay: 1,
     eventClick:(info)=>{
         info.jsEvent.preventDefault();
+        IntroModalVisible.value = true;
         let t = info.event
-        let c = IntroGen(t)
-        Modal.info({
-            title: '简介生成',
-            content: c,
-            modalStyle:{
-                whiteSpace: "pre-wrap"
-            },
-            hideCancel: true,
-            okText: '复制',
-            onOk: navigator.clipboard.writeText(c)
-        })
+        IntroModalLR.value = info.event.url
+        IntroModalContent.value = IntroGen(t)
     }
 }
 onMounted(()=>{
@@ -64,6 +58,26 @@ onMounted(()=>{
 <a-card title="飞书日程 (beta)" :loading="calendarLoading">
     <FullCalendar :options="calendarOptions"/>
 </a-card>
+<a-modal
+    :visible="IntroModalVisible"
+    title="简介生成"
+    ok-text="复制"
+    :mask-closable="true"
+    title-align="center"
+    status="info"
+    @cancel="IntroModalHide"
+    :simple="true"
+    :modal-style='{"white-space": "pre-wrap"}'
+>
+    <div><b>当日直播间</b></div>
+    <div>{{ IntroModalLR }}</div>
+    <div><b>简介</b></div>
+    <div>{{ IntroModalContent }}</div>
+    <template #footer>
+        <a-button @click="IntroModalLRCopy">复制直播间地址</a-button>
+        <a-button @click="IntroModalContentCopy" type="primary">复制简介</a-button>
+    </template>
+</a-modal>
 </template>
 
 <style scoped>
