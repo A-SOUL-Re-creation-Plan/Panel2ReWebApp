b5acfce85254be69ea67fb04a2df5469497a77c7
Just-Prog <JustProg5835@outlook.com>
2024-07-15 14:17:28 +0800

    凭据刷新: 修改 refresh_token 过期判断逻辑

--- src/utils/requests/uat_refresher.js
+++ src/utils/requests/uat_refresher.js
@@ -7,26 +7,33 @@ const user = storeToRefs(useUserStore())
 const headers = {
   "Panel2Re-RefreshAuthorization": `${user.refresh_token.value}`,
 };
+async function onUserAccessTokenExpired(){
+  console.log("refresh_token expired");
+  Modal.open({
+    title: "令牌过期",
+    content: "登录过期，请使用飞书SSO重新授权登录。",
+    onOk: () => {
+      useUserStore().reset();
+      router.push("/user/login");
+    },
+  });
+}
 async function checkUserAccessToken(){
   if (user.rt_expires.value <= Date.parse(new Date()) / 1000 || user.refresh_token.value.length <= 3) {
-    console.log('refresh_token expired')
-    Modal.open({
-      title: "令牌过期",
-      content: "登录过期，请使用飞书SSO重新授权登录。",
-      onOk: () => {
-        useUserStore().reset();
-        router.push("/user/login");
-      },
-    });
+    await onUserAccessTokenExpired()
   } else if (user.at_expires.value <= Date.parse(new Date()) / 1000 || user.token.value.length <= 3) {
     console.log("setting new token")
-    await requests.get("/api/lark_refresh_uat",{headers: headers}).then((resp) => {
+    requests.get("/api/lark_refresh_uat",{headers: headers}).then((resp) => {
       if (resp.data.code == 0) {
         user.token.value = resp.data.data.access_token;
         user.refresh_token.value = resp.data.data.refresh_token;
         user.rt_expires.value = resp.data.data.rt_expires_at;
         user.at_expires.value = resp.data.data.at_expires_at;
+      }else{
+        onUserAccessTokenExpired()
       }
+    }).catch(e=>{
+      onUserAccessTokenExpired()
     })
   }
 }
