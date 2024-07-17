45f5ae9ed58203344b03b895c3f0c04bee01e6e7
Just-Prog <JustProg5835@outlook.com>
2024-07-17 22:01:52 +0800

    快捷动态查看-live_rcmd: 修改数据类型为 reactive

--- src/components/Bilibili/DynamicDetails/LIVE_RCMD.vue
+++ src/components/Bilibili/DynamicDetails/LIVE_RCMD.vue
@@ -1,12 +1,13 @@
 <script setup>
+import { ref } from 'vue'
 import dateFormatter from '@/utils/dateFormatter'
 const props = defineProps(['data'])
-const data = JSON.parse(props.data.modules.module_dynamic.major.live_rcmd.content).live_play_info
+const data = ref(JSON.parse(props.data.modules.module_dynamic.major.live_rcmd.content).live_play_info)
 function go(room_id){
     let url = 'https://live.bilibili.com/'+room_id;
     window.open('javascript:window.name;', '<script>location.replace("'+url+'")<\/script>');
 }
-const live_start_time = dateFormatter(new Date(data.live_start_time*1000))
+const live_start_time = dateFormatter(new Date(data.value.live_start_time*1000))
 </script>
 
 <template>
