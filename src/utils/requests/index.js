b63293a63bc757b1c771ab0ee491652e7c00440b
Just-Prog <JustProg5835@outlook.com>
2024-08-04 20:36:49 +0800

    axios封装: 增加拦截器逻辑

--- src/utils/requests/index.js
+++ src/utils/requests/index.js
@@ -1,5 +1,27 @@
 import axios from "axios";
+import { Notification } from '@arco-design/web-vue'
+const networkErrNotification = (code) => {
+    Notification.error({
+        title: 'Network Error',
+        content: '错误代码: '+code,
+        duration: 5000
+    })
+}
 // import.meta.env.VITE_API_MOCKING && import("@/utils/requests/mock");
 const instance = axios.create()
+
+instance.interceptors.request.use(function (config) {
+    return config;
+  }, function (error) {
+    networkErrNotification(error.code);
+    return Promise.reject(error);
+  });
+
+instance.interceptors.response.use(function (response) {
+    return response;
+  }, function (error) {
+    networkErrNotification(error.code);
+    return Promise.reject(error);
+  });
 
 export default instance
