335ed7adb66a71ac8608360e0e7eed7e78bb7049
Just-Prog <JustProg5835@outlook.com>
2024-08-28 22:53:35 +0800

    动态快捷查看: 修复转发类动态没有点击事件的问题

--- src/components/Bilibili/DynamicDetails/FORWARD.vue
+++ src/components/Bilibili/DynamicDetails/FORWARD.vue
@@ -3,11 +3,15 @@ import DynamicDetail from '@/components/Bilibili/DynamicDetail.vue'
 const props = defineProps(['data'])
 const data = props.data.modules.module_dynamic.desc
 const orig = props.data.orig
+const dynamic_id = props.data.id_str
+function go(url){
+    window.open('javascript:window.name;', '<script>location.replace("'+url+'")<\/script>');
+}
 </script>
 
 <template>
     <a-card :bordered="false">
-        <div class="bili_dynamic_normal" v-html="data.text"/>
+        <div class="bili_dynamic_normal" v-html="data.text" @click="go('https://www.bilibili.com/opus/'+dynamic_id)"/>
         <span class="bili_dynamic_forwardflag">Forward from:</span>
         <DynamicDetail :content="orig"/>
     </a-card>
