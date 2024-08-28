1e32c6143a3fdf15a01e719fd8b1f0785a8acf39
Just-Prog <JustProg5835@outlook.com>
2024-08-28 22:59:16 +0800

    动态快捷查看: 修复LIVE_RCMD类无法正常跳转直播间的问题

--- src/components/Bilibili/DynamicDetails/LIVE_RCMD.vue
+++ src/components/Bilibili/DynamicDetails/LIVE_RCMD.vue
@@ -3,9 +3,8 @@ import { ref } from 'vue'
 import dateFormatter from '@/utils/dateFormatter'
 const props = defineProps(['data'])
 const data = ref(JSON.parse(props.data.modules.module_dynamic.major.live_rcmd.content).live_play_info)
-function go(room_id){
-    let url = 'https://live.bilibili.com/'+room_id;
-    window.open('javascript:window.name;', '<script>location.replace("'+url+'")<\/script>');
+function go(url){
+    window.open('javascript:window.name;', '<script>location.replace("https:'+url+'")<\/script>');
 }
 const live_start_time = dateFormatter(new Date(data.value.live_start_time*1000))
 </script>
@@ -13,14 +12,14 @@ const live_start_time = dateFormatter(new Date(data.value.live_start_time*1000))
 <template>
     <a-card :bordered="false">
         <template #cover>
-            <img :src="data.cover" referrerPolicy="no-referrer" @click="go(data.bvid)"/>
+            <img :src="data.cover" referrerPolicy="no-referrer" @click="go(data.link)"/>
         </template>
         <template #title>
             直播标题： {{ data.title }}
         </template>
         <template #actions>
             <span>开始于：{{ live_start_time }}</span>
-            <span class="icon-hover" @click="go(data.room_id)"> <IconShareInternal /> 进入直播间 </span>
+            <span class="icon-hover" @click="go(data.link)"> <IconShareInternal /> 进入直播间 </span>
         </template>
     </a-card>
 </template>
