e51f8163895bb268cb7394a85723489bb8135440
Just-Prog <JustProg5835@outlook.com>
2025-09-11 08:18:14 +0800

    fix: 哔哩哔哩改了带图动态的数据格式导致页面爆炸

--- src/components/Bilibili/DynamicDetails/DRAW.vue
+++ src/components/Bilibili/DynamicDetails/DRAW.vue
@@ -1,40 +1,49 @@
 <script setup>
-import { onBeforeMount } from 'vue';
+import { onBeforeMount } from "vue";
 
-const props = defineProps(['data'])
-
-
-const dynamic_id = props.data.id_str
-const data = props.data.modules.module_dynamic.desc
-const imgs = props.data.modules.module_dynamic.major.draw.items
-function go(url){
-    window.open('javascript:window.name;', '<script>location.replace("'+url+'")<\/script>');
+const props = defineProps(["data"]);
+const dynamic_id = props.data.id_str;
+const data = props.data.modules.module_dynamic.major.opus.summary;
+const imgs = props.data.modules.module_dynamic.major.opus.pics;
+function go(url) {
+  window.open(
+    "javascript:window.name;",
+    '<script>location.replace("' + url + '")<\/script>'
+  );
 }
 </script>
 
 <template>
-    <a-card :bordered="false">
-        <div class="bili_dynamic_normal" v-html="data.text" @click="go('https://www.bilibili.com/opus/'+dynamic_id)"/>
-        <br/>
-        <div class="bili_dynamic_drawimg">
-            <span class="bili_dynamic_drawimg_tips">
-                单击图片查看原图
-            </span>
-            <a-row :gutter="10">
-                <a-col :span="8" v-for="i in imgs">
-                    <img :src="i.src+'@400w_400h_1e_1c.png'" width="100%" @click="go(i.src)" class="bili_dynamic_drawimg_items" referrerPolicy="no-referrer"/>
-                </a-col>
-            </a-row>
-        </div>
-    </a-card>
+  <a-card :bordered="false">
+    <div
+      class="bili_dynamic_normal"
+      v-html="data.text ?? ''"
+      @click="go('https://www.bilibili.com/opus/' + dynamic_id)"
+    />
+    <br />
+    <div class="bili_dynamic_drawimg">
+      <span class="bili_dynamic_drawimg_tips"> 单击图片查看原图 </span>
+      <a-row :gutter="10">
+        <a-col :span="8" v-for="i in imgs">
+          <img
+            :src="i.url + '@400w_400h_1e_1c.png'"
+            width="100%"
+            @click="go(i.url)"
+            class="bili_dynamic_drawimg_items"
+            referrerPolicy="no-referrer"
+          />
+        </a-col>
+      </a-row>
+    </div>
+  </a-card>
 </template>
 
 <style>
-    .bili_dynamic_drawimg_items{
-        aspect-ratio: 1
-    }
-    .bili_dynamic_drawimg_tips{
-        font-size: 10px;
-        color: gray;
-    }
+.bili_dynamic_drawimg_items {
+  aspect-ratio: 1;
+}
+.bili_dynamic_drawimg_tips {
+  font-size: 10px;
+  color: gray;
+}
 </style>
