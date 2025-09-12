38693a1f4dfed2ae8683e144c2e90c53ed33086a
Just-Prog <JustProg5835@outlook.com>
2025-09-12 13:10:11 +0800

    fix: 专栏和纯文本动态解析变动

--- src/components/Bilibili/DynamicDetails/ARTICLE.vue
+++ src/components/Bilibili/DynamicDetails/ARTICLE.vue
@@ -1,32 +1,33 @@
 <script setup>
-const props = defineProps(['data'])
+const props = defineProps(["data"]);
 
+const data = props.data.modules.module_dynamic.major.opus;
 
-const data = props.data.modules.module_dynamic.major.article
-
-function go(url){
-    window.open('javascript:window.name;', '<script>location.replace("'+url+'")<\/script>');
+function go(url) {
+  window.open(
+    "javascript:window.name;",
+    '<script>location.replace("' + url + '")<\/script>'
+  );
 }
 </script>
 
 <template>
-
-    <a-card :bordered="false">
-        <div class="bili_dynamic_title">
-            {{ data.title }}
-        </div>
-        <div v-for="i in data.covers"> 
-            <img :src="i" referrerPolicy="no-referrer" @click="go(data.jump_url)"/>
-        </div>
-        <template #actions>
-            <span @click="go(data.jump_url)"> <IconShareInternal /> 查看全文 </span>
-        </template>
-        <div v-html="data.desc+'...'" @click="go(data.jump_url)"></div>
-    </a-card>
+  <a-card :bordered="false">
+    <div class="bili_dynamic_title">
+      {{ data.title }}
+    </div>
+    <div v-for="i in data.pics">
+      <img :src="i" referrerPolicy="no-referrer" @click="go(data.url)" />
+    </div>
+    <template #actions>
+      <span @click="go(data.jump_url)"> <IconShareInternal /> 查看全文 </span>
+    </template>
+    <div v-html="data.summary.text + '...'" @click="go(data.jump_url)"></div>
+  </a-card>
 </template>
 
 <style>
-    .bili_dynamic_datetime{
-        font-size: 12px
-    }
+.bili_dynamic_datetime {
+  font-size: 12px;
+}
 </style>
--- src/components/Bilibili/DynamicDetails/WORD.vue
+++ src/components/Bilibili/DynamicDetails/WORD.vue
@@ -1,21 +1,25 @@
 <script setup>
-const props = defineProps(['data'])
+const props = defineProps(["data"]);
 
+const data = props.data.modules.module_dynamic.major.opus.summary.text;
 
-const data = props.data.modules.module_dynamic.desc.text
-const dynamic_id = props.data.id_str
-function go(url){
-    window.open('javascript:window.name;', '<script>location.replace("'+url+'")<\/script>');
+const dynamic_id = props.data.id_str;
+function go(url) {
+  window.open(
+    "javascript:window.name;",
+    '<script>location.replace("' + url + '")<\/script>'
+  );
 }
 </script>
 
 <template>
-    <a-card :bordered="false">
-        <div class="bili_dynamic_normal" v-html="data" @click="go('https://www.bilibili.com/opus/'+dynamic_id)"/>
-    </a-card>
-    
+  <a-card :bordered="false">
+    <div
+      class="bili_dynamic_normal"
+      v-html="data"
+      @click="go('https://www.bilibili.com/opus/' + dynamic_id)"
+    />
+  </a-card>
 </template>
 
-<style>
-
-</style>
+<style></style>
